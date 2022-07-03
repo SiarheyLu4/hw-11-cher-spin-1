b8766883961c25f5336757b6d2200d361ca64d5a
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-07-03 21:20:37 +0300

    fin

---
+++ src/js/preloader.js
@@ -0,0 +1,12 @@
+
+
+
+
+document.body.onload = function () {
+    
+        const preLoader = document.getElementById('page-preloader');
+        if (!preLoader.classList.contains('done')) {
+            preLoader.classList.add('done');
+        }
+    
+}
